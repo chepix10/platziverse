5d80cc55a809b7391742838b0c7466304b1069f2
José Luis Morales Ruiz <jlmruiz@outlook.com>
2018-05-28 16:37:53 -0600

    Added api authorization with jwt

--- platziverse-api/api.js
+++ platziverse-api/api.js
@@ -3,6 +3,7 @@
 const debug = require('debug')('platziverse:api:routes')
 const express = require('express')
 const asyncify = require('express-asyncify')
+const auth = require('express-jwt')
 const db = require('platiziverse-db')
 const config = require('../platziverse-db/config')({ setup: false })
 const errors = require('./api-errors')
@@ -25,12 +26,22 @@ api.use('*', async (req, res, next) => {
   next()
 })
 
-api.get('/agents', async (req, res, next) => {
+api.get('/agents', auth({secret: 'platzi'}), async (req, res, next) => {
   debug('A request has come to /agents')
+
+  const { user } = req
+
+  if (!user || !user.username) {
+    return next(new errors.NotAuthorizedError())
+  }
 
   let agents = []
   try {
-    agents = await Agent.findConnected()
+    if (user.admin) {
+      agents = await Agent.findConnected()
+    } else {
+      agents = await Agent.findByUsername(user.username)
+    }
   } catch (e) {
     next(e)
   }
