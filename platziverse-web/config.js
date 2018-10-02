23f16de4ce22e76600b426d3dca2fb3015c79ca8
José Luis Morales Ruiz <jlmruiz@outlook.com>
2018-10-02 07:48:58 -0600

    Added UI for real time dashboard in Vue

---
+++ platziverse-web/config.js
@@ -0,0 +1,6 @@
+'use strict'
+
+module.exports = {
+  endpoint: process.env.API_ENDPOINT || 'http://localhost:3000',
+  apiToken: process.env.API_TOKEN || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InBsYXR6aSIsImFkbWluIjp0cnVlLCJwZXJtaXNzaW9ucyI6WyJtZXRyaWNzOnJlYWQiXSwiaWF0IjoxNTM4NDQ2NDgzfQ.RZvpE-CjobyV-Gp8PujNd5haQiAEqyzlPVV-zpgycOQ'
+}
